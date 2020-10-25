668bf9b5b24da49987199bcbdfc20fb08bb463cc
MLee314 <58196838+MLee314@users.noreply.github.com>
2020-10-24 21:56:45 -0400

    Revert "lab 6"
    This reverts commit f72024ff177acdeb4cff4bcc96832915c95b1722, reversing
    changes made to 89b51bdec16381019ad00f3070260cdb3028be87.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
-import countries from './public/lab_6/countries.js';
 
 dotenv.config();
 
@@ -14,12 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
@@ -28,7 +21,7 @@ app.route('/api')
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    res.json(countries);
+    res.send('hello world')
   });
 
 app.listen(port, () => {
